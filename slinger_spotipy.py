93bd0b173313f326648100691ef60ae153302650
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-15 18:49:37 +0300

    add: download tracks based on media

--- slinger_spotipy.py
+++ slinger_spotipy.py
@@ -28,7 +28,9 @@ song_url = input('Enter url of playlist or album: ')
 
 type_of_music = resolve_type_of_song(song_url)
 
+print('--------------------------------------------------------')
 print(f'Type of media --> {type_of_music}')
+print('--------------------------------------------------------')
 
 
 def mkdir_and_chdir_to_download(type_of_music):
@@ -49,9 +51,9 @@ def mkdir_and_chdir_to_download(type_of_music):
         download_to_folder) else None
 
     os.chdir(download_to_folder)
+    print('--------------------------------------------------------')
 
-# call function to create and change to this dir
-
+# call function to create and change to specified directory dir
 
 mkdir_and_chdir_to_download(type_of_music)
 
@@ -74,6 +76,7 @@ def getPlaylistTracks(link):
         tracks.append(f'{artist} {song}')
     return tracks
 
+
 def getAlbumTracks(link):
     """
     get a list of album tracks and the artist that created them 
@@ -92,82 +95,24 @@ def getAlbumTracks(link):
         tracks.append(f'{artist} {song}')
     return tracks
 
+
 def downloadTracks(tracks):
     for track in tracks:
-        match_song = str(VideosSearch(track, limit=1).result()) # search for song and return match
+        match_song = str(VideosSearch(track, limit=1).result()
+                         )  # search for song and return match
         song = YouTube(str(match_song))
         print(f'downloading {song.title}')
         stream = song.streams.filter(only_audio=True)
-        stream.first().download() # dowload audio
+        stream.first().download()  # dowload audio
         print('--------------------------------------------------------')
 
-
-if type_of_music == 'playlist':
-    playlist_tracks = getPlaylistTracks(song_url)
-    downloadTracks(playlist_tracks)
-elif type_of_music == 'album':
-    album_tracks = getAlbumTracks(song_url)
-    downloadTracks()
-
-# # # handle playlists
-# # def get_playlist_ids(user, playlist_id):
-# #     ids = []
-# #     playlist = sp.user_playlist(user, playlist_id)
-# #     for item in playlist['tracks']['items']:
-# #         track = item['track']
-# #         ids.append(track['id'])
-# #     return ids
+def downloadTracksBasedOnMediaType(type_of_music):
+    if type_of_music == 'playlist':
+        playlist_tracks = getPlaylistTracks(song_url)
+        downloadTracks(playlist_tracks)
+    elif type_of_music == 'album':
+        album_tracks = getAlbumTracks(song_url)
+        downloadTracks(album_tracks)
 
 
-# if album_playlist_bool == 'y':
-#     tracks = get_album_tracks(song_url)
-#     for song in tracks:
-#         search_song = VideosSearch(song, limit=1)
-#         result = search_song.result()
-#         song = YouTube(str(result))
-#         print(f'downloading {song.title} ')
-#         stream = song.streams.filter(only_audio=True)
-#         print('--------------------------------------------------------')
-#         stream.first().download()
-#     sys.exit(1)
-
-
-# else:
-#     ids = getTrackIDs('', playlist_url)
-
-
-# def getAlbumId(album_id):
-#     ids = []
-#     album = sp.albums(album_id)
-
-
-# def getTrackFeatures(id):
-#     meta = sp.track(id)
-#     # meta
-#     name = meta['name']
-#     album = meta['album']['name']
-#     artist = meta['album']['artists'][0]['name']
-
-#     track = [name+' '+artist]
-#     return track
-
-# # loop over track ids
-
-
-# for i in range(len(ids)):
-#     track = getTrackFeatures(ids[i])
-#     tracks.append(track)
-
-
-# # youtube search and download
-
-# for details in tracks:
-#     for song in details:
-#         song = str(song)
-#         search_song = VideosSearch(song, limit=1)
-#         result = search_song.result()
-#         song = YouTube(str(result))
-#         print(f'downloading {song.title} ')
-#         stream = song.streams.filter(only_audio=True)
-#         print('--------------------------------------------------------')
-#         stream.first().download()
+downloadTracksBasedOnMediaType(type_of_music)
