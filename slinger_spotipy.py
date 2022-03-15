b842195439ee67968cfe240ae4764151a8e818b3
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-03-15 18:40:36 +0300

    add: dowload function to dowload tracks based on the type of music

--- slinger_spotipy.py
+++ slinger_spotipy.py
@@ -33,7 +33,7 @@ print(f'Type of media --> {type_of_music}')
 
 def mkdir_and_chdir_to_download(type_of_music):
     """
-    create a directory to download music in based on the type of music.
+    create a directory to download music  based on the type of music.
     After creating the directory,change to that directory
 
     Args:
@@ -56,93 +56,118 @@ def mkdir_and_chdir_to_download(type_of_music):
 mkdir_and_chdir_to_download(type_of_music)
 
 
-def getPlaylist(link):
-    """ get a list of playlist music
+def getPlaylistTracks(link):
+    """ get a list of playlist tracks and the artist that created them 
 
     Args:
         link (string): spotify playlist link
+    Returns:
+            list : a list of playlist songs and artists 
     """
     tracks = []
     playlist = sp.playlist_items(link)
     df = pd.DataFrame(playlist)
     for i in df.index:
         items = df.loc[i, 'items']
-        altist = items['track']['album']['artists'][0]['name']
+        artist = items['track']['album']['artists'][0]['name']
         song = items['track']['album']['name']
-        tracks.append(f'{altist} {song}')
+        tracks.append(f'{artist} {song}')
+    return tracks
 
-
-def getAlbum(link):
+def getAlbumTracks(link):
+    """
+    get a list of album tracks and the artist that created them 
+    Args:
+        link (string): spotify album link
+    Returns:
+            list : a list of album songs and artists 
+    """
     tracks = []
     album = sp.album_tracks(link)
     data_flame = pd.DataFrame(album)
     for i in range(len(data_flame)):
         items = data_flame.loc[i]['items']
-        altist = items['artists'][0]['name']
+        artist = items['artists'][0]['name']
         song = items['name']
-        tracks.append(f'{altist} {song}')
+        tracks.append(f'{artist} {song}')
     return tracks
 
-
-# handle playlists
-def get_playlist_ids(user, playlist_id):
-    ids = []
-    playlist = sp.user_playlist(user, playlist_id)
-    for item in playlist['tracks']['items']:
-        track = item['track']
-        ids.append(track['id'])
-    return ids
-
-
-if album_playlist_bool == 'y':
-    tracks = get_album_tracks(song_url)
-    for song in tracks:
-        search_song = VideosSearch(song, limit=1)
-        result = search_song.result()
-        song = YouTube(str(result))
-        print(f'downloading {song.title} ')
+def downloadTracks(tracks):
+    for track in tracks:
+        match_song = str(VideosSearch(track, limit=1).result()) # search for song and return match
+        song = YouTube(str(match_song))
+        print(f'downloading {song.title}')
         stream = song.streams.filter(only_audio=True)
+        stream.first().download() # dowload audio
         print('--------------------------------------------------------')
-        stream.first().download()
-    sys.exit(1)
 
 
-else:
-    ids = getTrackIDs('', playlist_url)
+if type_of_music == 'playlist':
+    playlist_tracks = getPlaylistTracks(song_url)
+    downloadTracks(playlist_tracks)
+elif type_of_music == 'album':
+    album_tracks = getAlbumTracks(song_url)
+    downloadTracks()
+
+# # # handle playlists
+# # def get_playlist_ids(user, playlist_id):
+# #     ids = []
+# #     playlist = sp.user_playlist(user, playlist_id)
+# #     for item in playlist['tracks']['items']:
+# #         track = item['track']
+# #         ids.append(track['id'])
+# #     return ids
 
 
-def getAlbumId(album_id):
-    ids = []
-    album = sp.albums(album_id)
+# if album_playlist_bool == 'y':
+#     tracks = get_album_tracks(song_url)
+#     for song in tracks:
+#         search_song = VideosSearch(song, limit=1)
+#         result = search_song.result()
+#         song = YouTube(str(result))
+#         print(f'downloading {song.title} ')
+#         stream = song.streams.filter(only_audio=True)
+#         print('--------------------------------------------------------')
+#         stream.first().download()
+#     sys.exit(1)
 
 
-def getTrackFeatures(id):
-    meta = sp.track(id)
-    # meta
-    name = meta['name']
-    album = meta['album']['name']
-    artist = meta['album']['artists'][0]['name']
-
-    track = [name+' '+artist]
-    return track
-
-# loop over track ids
+# else:
+#     ids = getTrackIDs('', playlist_url)
 
 
-for i in range(len(ids)):
-    track = getTrackFeatures(ids[i])
-    tracks.append(track)
+# def getAlbumId(album_id):
+#     ids = []
+#     album = sp.albums(album_id)
 
 
-# youtube search and download
+# def getTrackFeatures(id):
+#     meta = sp.track(id)
+#     # meta
+#     name = meta['name']
+#     album = meta['album']['name']
+#     artist = meta['album']['artists'][0]['name']
 
-for details in tracks:
-    for song in details:
-        song = str(song)
-        search_song = VideosSearch(song, limit=1)
-        result = search_song.result()
-        song = YouTube(str(result))
-        print(f'downloading {song.title} ')
-        stream = song.streams.filter(only_audio=True)
-        print('--------------------------------------------------------')
-        stream.first().download()
+#     track = [name+' '+artist]
+#     return track
+
+# # loop over track ids
+
+
+# for i in range(len(ids)):
+#     track = getTrackFeatures(ids[i])
+#     tracks.append(track)
+
+
+# # youtube search and download
+
+# for details in tracks:
+#     for song in details:
+#         song = str(song)
+#         search_song = VideosSearch(song, limit=1)
+#         result = search_song.result()
+#         song = YouTube(str(result))
+#         print(f'downloading {song.title} ')
+#         stream = song.streams.filter(only_audio=True)
+#         print('--------------------------------------------------------')
+#         stream.first().download()
