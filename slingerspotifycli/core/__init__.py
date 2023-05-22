3bca9c0695bbc6c74b78b3fa92c0e7014aa3f250
kris <krisdjangodeveloper@gmail.com>
2023-05-22 07:54:42 +0300

    update: files

---
+++ slingerspotifycli/core/__init__.py
@@ -0,0 +1,1 @@
+import slinger_spotipy
---
+++ slingerspotifycli/core/slinger_spotipy.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python
+
+# author Chris muiru
+
+# title: _spotify music downloader
+
+import os
+import spotipy
+import pandas as pd
+from pytube import YouTube
+from spotipy.oauth2 import SpotifyClientCredentials
+from youtubesearchpython import VideosSearch
+from type_of_song import resolve_type_of_song
+cid = os.getenv('SpotifyId')
+secret = os.getenv('SpotifySecret')
+
+# authenticate using credentials provided
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+song_url = input('Enter url of playlist or album: ')
+
+type_of_music = resolve_type_of_song(song_url)
+
+print('--------------------------------------------------------')
+print(f'Type of media --> {type_of_music}')
+print('--------------------------------------------------------')
+
+
+def mkdir_and_chdir_to_download(type_of_music):
+    """
+    create a directory to download music  based on the type of music.
+    After creating the directory,change to that directory
+
+    Args:
+        type_of_music (string): type of music media e.g show,playlist....
+    """
+    MUSIC_DIR = os.path.expanduser("~")  # get home directory path
+    song_folder = input('enter folder name: ')
+
+    download_to_folder = os.path.join(
+        MUSIC_DIR, 'Music', f'{type_of_music}s', song_folder)
+    if not os.path.exists(download_to_folder):
+        os.makedirs(download_to_folder)
+
+    os.chdir(download_to_folder)
+    print('--------------------------------------------------------')
+
+
+# call function to create and change to specified directory dir
+mkdir_and_chdir_to_download(type_of_music)
+
+
+def getPlaylistTracks(link):
+    """ get a list of playlist tracks and the artist that created them
+
+    Args:
+        link (string): spotify playlist link
+    Returns:
+            list : a list of playlist songs and artists
+    """
+    tracks = []
+    playlist = sp.playlist_items(link)
+    df = pd.DataFrame(playlist)
+    for i in df.index:
+        items = df.loc[i, 'items']
+        artist = items['track']['album']['artists'][0]['name']
+        song = items['track']['album']['name']
+        tracks.append(f'{artist} {song}')
+    return tracks
+
+
+def getAlbumTracks(link):
+    """
+    get a list of album tracks and the artist that created them
+    Args:
+        link (string): spotify album link
+    Returns:
+            list : a list of album songs and artists
+    """
+    tracks = []
+    album = sp.album_tracks(link)
+    data_flame = pd.DataFrame(album)
+    for i in range(len(data_flame)):
+        items = data_flame.loc[i]['items']
+        artist = items['artists'][0]['name']
+        song = items['name']
+        tracks.append(f'{artist} {song}')
+    return tracks
+
+
+def downloadVideoOrAudio(song, choice):
+    try:
+        print(f'downloading {song.title}')
+        if choice == 'audio':
+            stream = song.streams.filter(only_audio=True).first(
+            ).download()  # dowload first audio file
+        elif choice == 'video':
+            stream = song.streams.filter(
+                progressive=True, file_extension="mp4", res="720p").first().download()
+    except Exception as e:
+        print(e)
+
+
+def downloadTracks(tracks):
+    """
+    download the music in tracks parameter
+
+    Args:
+        tracks (list): tracks to download from getTrack functions specified
+
+    """
+    choice = input('audio or video?: ')
+    print('--------------------------------------------------------')
+    for track in tracks:
+        match_song = str(VideosSearch(track, limit=1).result()
+                         )  # search for song and return match
+
+        try:
+            song = YouTube(str(match_song))
+        except Exception as e:
+            pass
+
+        downloadVideoOrAudio(song, choice)
+        print('--------------------------------------------------------')
+
+
+def downloadTracksBasedOnMediaType(type_of_music):
+    """
+    checks the type of song and invoke the downloadTracks based on song type
+
+    Args:
+        type_of_music (str): the type of song
+    """
+    if type_of_music == 'playlist':
+        playlist_tracks = getPlaylistTracks(song_url)
+        downloadTracks(playlist_tracks)
+    elif type_of_music == 'album':
+        album_tracks = getAlbumTracks(song_url)
+        downloadTracks(album_tracks)
+
+
+downloadTracksBasedOnMediaType(type_of_music)
